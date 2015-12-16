a7804f8a82e235244894e871c301cccb6e61b45d
zhanglei <zhanglei1239@126.com>
2015-12-16 16:10:41 +0800

    Initial Commit

---
+++ NetWorkingDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetWorkingDemo
+//
+//  Created by Mac on 15/12/16.
+//  Copyright © 2015年 lei.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
